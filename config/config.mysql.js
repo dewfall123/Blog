e6575e173d0fb6df839dd058dc13dcc3afbf2167
dewfall123 <1334767234@qq.com>
2018-11-11 21:48:18 +0800

    修改

--- config/config.mysql.js
+++ config/config.mysql.js
@@ -8,9 +8,9 @@ exports.mysql = {
         // 端口号
         port: '3306',
         // 用户名
-        user: '',
+        user: 'zuofenghua',
         // 密码
-        password: '',
+        password: '614035',
         // 数据库名
         database: 'blog',
     },

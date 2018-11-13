0cd6a9574fd8a4e443e694d89953880fcb9e83b8
dewfall123 <1334767234@qq.com>
2018-11-13 22:13:41 +0800

    修改

--- config/config.default.js
+++ config/config.default.js
@@ -61,9 +61,9 @@ module.exports = appInfo => {
             // 端口号
             port: '3306',
             // 用户名
-            user: 'zuofenghua',
+            user: '',
             // 密码
-            password: '614035',
+            password: '',
             // 数据库名
             database: 'blog',
         },
